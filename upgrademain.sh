df77e9f17748ed270cf7b5db2f2019ff2611ce7e
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-01-02 18:46:13 +0200

    Create upgrademain.sh

---
+++ upgrademain.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd $HOME
+wget -O kyved https://github.com/KYVENetwork/chain/releases/download/v1.5.0/kyved_mainnet_linux_amd64
+chmod +x kyved
+mv kyved $(which kyved)
+kyved version
+
+systemctl restart kyved && journalctl -fu kyved -o cat
