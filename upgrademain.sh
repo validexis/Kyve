fe1e6c075ea5b9a1d75ad18edf27e3fdfd3d094d
VALIDEXIS <142050563+validexis@users.noreply.github.com>
2025-04-04 22:49:59 +0300

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $HOME
-wget -O kyved https://github.com/KYVENetwork/chain/releases/download/v1.5.0/kyved_mainnet_linux_amd64
+wget -O kyved https://github.com/KYVENetwork/chain/releases/download/v2.0.0/kyved_mainnet_linux_amd64
 chmod +x kyved
 mv kyved $(which kyved)
 kyved version
